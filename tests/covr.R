cbc0b753f749563530ad876ee885e8dcb478b9d1
Marton Kolossvary <mkolossvary@mgh.harvard.edu>
2023-03-31 15:40:06 +0000

    version 1.0.1

--- tests/covr.R
+++ tests/covr.R
@@ -1,5 +1,6 @@
-# library(covr)
-# coverage <- covr::package_coverage(path="/Users/mjk2/Dropbox (Personal)/R_packages/parseRPDR/")
+library(covr)
+# remove: --as-cran and rebuild
+# coverage <- covr::package_coverage(path="/Users/mjk2/Dropbox (Personal)/R_packages/parseRPDR/", quiet = FALSE)
 # coverage
 # report(coverage)
 # codecov(token = "YOUR_TOKEN_HERE")
